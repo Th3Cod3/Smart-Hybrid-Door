5af7683f694c46d84d778ccc3149a38282355f3d
Th3-Cod3 <th3-cod3-1230@hotmail.com>
2019-03-12 08:59:17 -0400

    Change struture and add RFID

--- SmartHybridDoor.ino
+++ SmartHybridDoor.ino
@@ -1,52 +1,64 @@
-
 #include <Adafruit_Fingerprint.h>
 #include <LiquidCrystal.h>
 #include <SoftwareSerial.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+// error code
 #define FINGERPRINT_EXIST 0x20
 #define FINGERPRINT_NOMASTER 0x21
 
-const int
-	// buttons
-	ENROLL_SWITCH = A1,
-	VERIFY_SWITCH = A2,
-	DELETE_SWITCH = A3,
-	BACKDOOR_SWITCH = A0,
-	// SPI 
-	PIN_SS = A4,
-	PIN_RST = 10,
-	//LCD
-	ENABLE = 9,
-	READ_SET = 8,
-	DATA_4 = 7,
-	DATA_5 = 6,
-	DATA_6 = 5,
-	DATA_7 = 4;
-boolean 
-	// status
-	enrollSwitchStatus = false, 
-	verifySwitchStatus = false, 
-	deleteSwitchStatus = false,
-	backdoorSwitchStatus = false;
+// BUTTONS
+#define UP_BUTTON 19 // green wire
+#define DOWN_BUTTON 20 // grey wire
+#define BACK_BUTTON 21 // red wire
+#define ENTER_BUTTON 18 // black wire
+
+// RFID SPI PINS
+#define SS_PIN 10
+#define RST_PIN A4
+
+// LCD PINS
+#define ENABLE 9
+#define READ_SET 8
+#define DATA_4 7
+#define DATA_5 6
+#define DATA_6 5
+#define DATA_7 4
+
+int lcdCursor = 0;
+int lcdCursorCharPosition = 0;
+const String lcdCursorChar = "*";
+// function init
+void printError(uint8_t errorCode);
+uint8_t getFingerId();
+uint8_t fingerEnroll(uint8_t socketId);
+int checkButton(uint16_t time);
+
+// Module objects
 LiquidCrystal lcd(READ_SET, ENABLE, DATA_4, DATA_5, DATA_6, DATA_7);
 SoftwareSerial mySerial(2, 3);
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+MFRC522 mfrc522(SS_PIN, RST_PIN); 
 
+// global varaible
 uint8_t id;
 uint8_t p = -1;
+
 void setup() {
-	pinMode(VERIFY_SWITCH,INPUT);
-	pinMode(DELETE_SWITCH,INPUT);
-	pinMode(ENROLL_SWITCH,INPUT);
-	pinMode(BACKDOOR_SWITCH,INPUT);
-	Serial.begin(9600);	// Init Serial
-	SPI.begin();	// Init SPI bus
-	mfrc522.PCD_Init();	// Init MFRC522
+	Serial.begin(9600);
 	lcd.begin(16,2);	
 	lcd.print("Starting...");
+	pinMode(DOWN_BUTTON,INPUT);
+	pinMode(BACK_BUTTON,INPUT);
+	pinMode(UP_BUTTON,INPUT);
+	pinMode(ENTER_BUTTON,INPUT);
+	SPI.begin();	// Init SPI bus
+	mfrc522.PCD_Init();	// Init MFRC522
 	delay(500);
 	finger.begin(57600);
-	lcd.clear();
 
+	lcd.clear();
 	while(!finger.verifyPassword()){
 		lcd.print("Check password");
 		lcd.setCursor(0,1);
@@ -57,104 +69,88 @@ void setup() {
 	delay(500);
 }
 void loop() {
-	while(finger.templateCount == 0 || ENROLL_SWITCH == LOW){
-		lcd.clear();
-		lcd.print("    Welcome");
-		lcd.setCursor(0,1);
-		lcd.print("SmartHybridDoor");
-		if(checkOptions(2000))
-			break;
-		lcd.clear();
-		lcd.print("For the first");
-		lcd.setCursor(0,1);
-		lcd.print("time you need");
-		if(checkOptions(2000))
-			break;
-		lcd.clear();
-		lcd.print("to register the");
-		lcd.setCursor(0,1);
-		lcd.print("MASTER finger.");
-		if(checkOptions(2000))
-			break;
-		lcd.clear();
-		lcd.print("Press 8 for");
-		lcd.setCursor(0,1);
-		lcd.print("enroll");
-		if(checkOptions(4000))
-			break;
-	}
-	while(!checkOptions(2000)){
-		lcd.clear();
-		lcd.print("8 FOR ENROLL");
-		lcd.setCursor(0,1);
-		lcd.print("4 TO VERIFY");
-		if(checkOptions(2000))
-			break;
-		lcd.clear();
-		lcd.print("1 DELETE ALL");
-		lcd.setCursor(0,1);
-		lcd.print("TOTAL FINGER ");
-		lcd.print(finger.templateCount);
-		if(checkOptions(2000))
-			break;
-	}
+	// register master
+		// while(finger.templateCount == 0){
+		// 	lcd.clear();
+		// 	lcd.print("    Welcome");
+		// 	lcd.setCursor(0,1);
+		// 	lcd.print("SmartHybridDoor");
+		// 	if(checkOptions(2000))
+		// 		break;
+		// 	lcd.clear();
+		// 	lcd.print("For the first");
+		// 	lcd.setCursor(0,1);
+		// 	lcd.print("time you need");
+		// 	if(checkOptions(2000))
+		// 		break;
+		// 	lcd.clear();
+		// 	lcd.print("to register the");
+		// 	lcd.setCursor(0,1);
+		// 	lcd.print("MASTER finger.");
+		// 	if(checkOptions(2000))
+		// 		break;
+		// 	lcd.clear();
+		// 	lcd.print("Press 8 for");
+		// 	lcd.setCursor(0,1);
+		// 	lcd.print("enroll");
+		// 	if(checkOptions(4000))
+		// 		break;
+	// }
 
-	if(enrollSwitchStatus){
-		if(finger.templateCount == 0)
-			printError(fingerEnroll(1));
-		else{
-			lcd.clear();
-			lcd.print("Need MASTER"); 
-			lcd.setCursor(0,1);
-			lcd.print("Finger");
-			delay(2000);
-			getFingerId();
-			if(finger.fingerID == 1)
-				printError(fingerEnroll(finger.templateCount+1));
-			else
-				printError(FINGERPRINT_NOMASTER);
+	while(true){
+		lcd.clear();
+		String options[] = {"Enroll", "Delete", "Details", "Working"};
+		int counter = 0;
+		if(true){
+			for(int i = lcdCursor; counter < 2; i++){
+				lcd.print(options[i]);
+				lcd.setCursor(0,1);
+				counter++;
+			}
+			lcd.setCursor(15,lcdCursorCharPosition);
+			lcd.print(lcdCursorChar);
+			int buttonStatus = checkButton(100);
+
+			switch (buttonStatus){
+				case UP_BUTTON:
+					if(lcdCursorCharPosition == 0)
+						lcdCursor--;
+					else if(lcdCursorCharPosition == 1)
+						lcdCursorCharPosition = 0;
+					break;
+				case DOWN_BUTTON:
+					if(lcdCursorCharPosition == 1)
+						lcdCursor++;
+					else if(lcdCursorCharPosition == 0)
+						lcdCursorCharPosition = 1;
+					break;
+				case ENTER_BUTTON:
+					/* code */
+					break;
+				case BACK_BUTTON:
+					/* code */
+					break;
+			}
 		}
-	}else if(backdoorSwitchStatus){
-		printError(fingerEnroll(1));
-	}else if(deleteSwitchStatus){
-		lcd.clear();
-		lcd.print("Need MASTER"); 
-		lcd.setCursor(0,1);
-		lcd.print("Finger");
-		delay(2000);
-		getFingerId();
-		if(finger.fingerID == 1){
-			finger.emptyDatabase();
-			finger.getTemplateCount();
-		}else
-			printError(FINGERPRINT_NOMASTER);
-	}else if(verifySwitchStatus){
-		int response = getFingerId();
-		if(response == FINGERPRINT_OK){
-			lcd.clear();
-			lcd.print("Unlocked"); 
-			lcd.setCursor(0,1);
-			lcd.print("ID: "); lcd.print(finger.fingerID);
-			delay(2000);
-		}else
-			printError(response);
 	}
-
-	lcd.clear();
-	lcd.print("Finish!");
-	delay(1000);
 }
 
-boolean checkOptions(uint16_t time){
-	for(int i = 0; i <= time; i++){
-		if (digitalRead(VERIFY_SWITCH) || digitalRead(DELETE_SWITCH) || digitalRead(ENROLL_SWITCH) || digitalRead(BACKDOOR_SWITCH)){
-			verifySwitchStatus = digitalRead(VERIFY_SWITCH);
-			deleteSwitchStatus = digitalRead(DELETE_SWITCH);
-			enrollSwitchStatus = digitalRead(ENROLL_SWITCH);
-			backdoorSwitchStatus = digitalRead(BACKDOOR_SWITCH);
-			return true;
-		}
+int checkButton(uint16_t time){
+	Serial.print("start    ");
+	Serial.println(time);
+
+	for(uint16_t i = 0; i <= time; i++){
+		Serial.print("Counter: ");
+		Serial.println(i);
 		delay(1);
+		if(digitalRead(UP_BUTTON))
+			return UP_BUTTON;
+		else if(digitalRead(DOWN_BUTTON))
+			return DOWN_BUTTON;
+		else if(digitalRead(BACK_BUTTON))
+			return BACK_BUTTON;
+		else if(digitalRead(ENTER_BUTTON))
+			return ENTER_BUTTON;
 	}
 	return false;
 }
@@ -258,5 +254,6 @@ void printError(uint8_t errorCode){
 		lcd.clear();
 		return;
 	}
-	return FINGERPRINT_OK;
+	return;
 }
+
